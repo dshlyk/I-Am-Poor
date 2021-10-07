fed6e3e45e76aff0a1de170095f38f7ab07e99cb
Шлык Дмитрий Юрьевич <Shlyk.D.Y@sberbank.ru>
2021-10-07 10:19:55 +0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 17876371 on 07.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
